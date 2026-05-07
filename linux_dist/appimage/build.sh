a11cf2a29369c6a77fcacd26f540a413bf8c9660
Rubin Bhandari <roobin.bhandari@gmail.com>
2026-05-08 04:28:30 +0545

    refactor: improve docker run command structure for better readability

--- linux_dist/appimage/build.sh
+++ linux_dist/appimage/build.sh
@@ -2,5 +2,18 @@
 set -Ceufox pipefail
 
 docker build -t bauh-appimage .
-docker run -e BAUH_VERSION=$BAUH_VERSION -v ./AppImageBuilder.yml:/build/AppImageBuilder.yml --rm --cap-add=SYS_ADMIN --device /dev/fuse --mount type=bind,source="$(pwd)",target=/build bauh-appimage
-# volume required to run tests: -v /var/run/docker.sock:/var/run/docker.sock
+
+docker_args=(
+	-e "BAUH_VERSION=${BAUH_VERSION}"
+	-e "APPIMAGE_EXTRACT_AND_RUN=1"
+	-v ./AppImageBuilder.yml:/build/AppImageBuilder.yml
+	--rm
+	--mount type=bind,source="$(pwd)",target=/build
+)
+
+if [[ -e /dev/fuse ]]; then
+	docker_args+=(--cap-add=SYS_ADMIN --device /dev/fuse)
+fi
+
+docker run "${docker_args[@]}" bauh-appimage
+# to run AppImageBuilder tests from inside the container, also mount: -v /var/run/docker.sock:/var/run/docker.sock
